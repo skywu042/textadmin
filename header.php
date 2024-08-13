4e53203fe4aa617cd054c68484b2794eabfea2a6
skywu042 <134019653+skywu042@users.noreply.github.com>
2024-08-13 17:37:11 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 25px;text-decoration: solid;  text-align: center;  text-decoration-color: brown;">INSTINCT SEEKERS CAR RENTAL PORTAL-<b><u>ADMIN PANEL</u></b></a> 
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ leftbar.php
@@ -0,0 +1,27 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">	tool Bar</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Add new Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Cars</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Car</a></li>
+						<li><a href="manage-vehicles.php">Manage cars</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+				<li><a href="addlocation.php"><i class="fa fa-files-o"></i> Add New Location</a></li>
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Feedbacks</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+			
+			</ul>
+		</nav>
